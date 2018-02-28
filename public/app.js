d2c96ad016863dd817c9c97a0c7edbb785b24db1
Ari Vidal <ariel.m.vidal@gmail.com>
2018-02-27 20:55:46 -0500

    making minor changes and fixing links

--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@ $.getJSON("/articles", function(data) {
   // For each one
   for (var i = 0; i < data.length; i++) {
     // Display the apropos information on the page
-      $("#articles").append(`<div class="divider"></div><div class='section'><p data-id="${data[i]._id}">${data[i].title}<br />${data[i].link}</p></div>`)
+      $("#articles").append(`<div class="divider"></div><div class='section'><p data-id="${data[i]._id}">${data[i].title}<br /><a class="articlelink" href="${data[i].link}" target=_blank>${data[i].link}</a></p></div>`)
   }
 });
 
